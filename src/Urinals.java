b90d06c6c632ffc11b571efa9ff75b3e5b64ff26
Krutik Pandya <112588253+kpandya8@users.noreply.github.com>
2022-10-27 16:13:16 -0700

    Skeleton Code with functions and file structure

--- src/Urinals.java
+++
@@ -1,7 +0,0 @@
-//Authour: Krutik Pandya 1225833793
-
-public class Urinals {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/urinalsTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class urinalsTest {
+    //Testcases
+    @Test
+    public void SampleTest1() {
+
+    }
+}
